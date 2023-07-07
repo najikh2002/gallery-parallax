bd186b3fc108a7173f3653b8f76774f4acb8b16e
Hizbullah Najihan <hizbullahnajihan@192.168.1.2>
2023-07-07 23:43:27 +0700

    gallery-parallax

--- app.js
+++ app.js
@@ -2,41 +2,42 @@ $(document).ready(function() {
     $(window).scroll(() => {
         var wScroll = $(this).scrollTop();
 
+        console.log(wScroll)
+    
         $('.title').css({
             'transform' : 'translate(0px,'+ wScroll/2 +'px)'
         });
     });
 
+
     $(window).scroll(function() {
-        var windowHeight = $(window).height();
-        var scrollTop = $(window).scrollTop();
-
-        $('[id="target"]').each(function() {
-            var targetTop = $(this).offset().top;
-            var targetBottom = targetTop + $(this).outerHeight();
-
-            if (targetTop < scrollTop + windowHeight && targetBottom > scrollTop) {
-                var scrollPos = (scrollTop - targetTop) / ($(this).outerHeight() - windowHeight) * 100;
-                
-                if ($(window).width() > 1000) {
-                    scrollPos = -scrollPos; // Mengubah scrollPos menjadi negatif
-                }
-                
-                var backgroundPos = "center " + scrollPos + "px";
-                $(this).css("background-position", backgroundPos);
-            }
-        });
+      var windowHeight = $(window).height();
+      var scrollTop = $(window).scrollTop();
+  
+      $('[class="content"]').each(function() {
+        var targetTop = $(this).offset().top;
+        var targetBottom = targetTop + $(this).outerHeight();
+  
+        if (targetTop < scrollTop + windowHeight && targetBottom > scrollTop) {
+          var scrollPos = (scrollTop - targetTop) / (windowHeight + $(this).outerHeight()) * 100;
+          
+          if ($(window).width() > 1000) {
+            scrollPos = scrollPos;
+          }
+          
+          var translateY = scrollPos*4 ; // Menyesuaikan kecepatan pergerakan
+          $(this).find('img').css('transform', 'translateY(' + translateY + 'px)');
+        }
+      });
     });
-});
-
-
+  });
 
 gsap.fromTo('h1', {opacity: 2 }, {
     opacity: 0,
     scrollTrigger: {
         trigger: 'h1',
         start: 'top top',
-        end: '420',
+        end: '350',
         scrub: 1,
     }
 });
@@ -46,3 +47,4 @@ gsap.fromTo('.img-grid', {opacity: 0 }, {
     duration: 5,
     delay: 2
 });
+  
